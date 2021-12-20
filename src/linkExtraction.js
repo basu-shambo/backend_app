a938b92e68d2c948a3e70b2a532085bfd8fd9b4f
basu-shambo <basushambo@outlook.com>
2021-12-20 09:19:29 +0530

    completed link extraction logic

--- src/linkExtraction.js
+++ src/linkExtraction.js
@@ -2,13 +2,17 @@ import linkModel from "../models/linkModel.js";
 
 export const extractLinks = (mongoClient,$)=>{
     let links = [];
-    let i=0
     $('a').each((index,link)=>{
-        console.log(link.hostName)
+        let url = $(link).attr('href');
+        if(urlRegexMatchindng(url)) links.push($);
     })
     return links;
 }
 
-function link_is_external(link_element) {
-    return (link_element.host !== global.location.host);
+
+const urlRegexMatchindng= (link) => {
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    return regex.test(link);
+
 }
